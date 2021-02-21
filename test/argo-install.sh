b257f06729e7da44125e6a2d1e3952d92247bc5f
kati <k.lassila-perini@cern.ch>
2021-02-21 20:58:49 +0100

    Add test

---
+++ test/argo-install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Install argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/stable/manifests/quick-start-postgres.yaml
+curl -sLO https://github.com/argoproj/argo/releases/download/v2.11.1/argo-linux-amd64.gz
+gunzip argo-linux-amd64.gz
+chmod +x argo-linux-amd64
+sudo mv ./argo-linux-amd64 /usr/local/bin/argo
